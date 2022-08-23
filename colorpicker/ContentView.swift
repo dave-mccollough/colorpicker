a125052523ac6afc6d11fef81ba30753b8cf2415
Dave McCollough <davemccollough@gmail.com>
2022-08-22 20:21:11 -0500

    Initial Commit

---
+++ colorpicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  colorpicker
+//
+//  Created by Dave McCollough on 8/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ colorpicker/colorpickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorpickerApp.swift
+//  colorpicker
+//
+//  Created by Dave McCollough on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct colorpickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
